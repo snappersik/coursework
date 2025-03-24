b0d5d4af0e5c131b19d528a389f0eea9549a5f3a
stud <sdr@af.yg>
2025-03-24 12:50:55 +0300

    del orderItem

--- src/main/java/com/almetpt/coursework/bookclub/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.almetpt.coursework.bookclub.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends GenericRepository<OrderItem> {
-}
-
